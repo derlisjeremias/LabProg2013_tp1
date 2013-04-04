67cadedc70875601cdb4ae3516465b68d51dcd3b
Jere <Jere@Jere-PC>
2013-04-04 00:54:13 -0300

    Primera actualizacion

--- src/ar/edu/untdf/labprog/tp1/ejer1/Sql.java
+++ src/ar/edu/untdf/labprog/tp1/ejer1/Sql.java
@@ -1,5 +1,5 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this template,  Tools | Templates
  * and open the template in the editor.
  */
 package ar.edu.untdf.labprog.tp1.ejer1;
